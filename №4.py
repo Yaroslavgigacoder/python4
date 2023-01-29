7a42439e9eb06cc8ed14326e3996494940a937d1
Yaroslav.Gladkov <gladkov2000@inbox.ru>
2023-01-29 19:02:15 +0300

    task4

--- №4.py
+++ №4.py
@@ -6,13 +6,12 @@ for i in range (N):
     age = int(input())
     age_list.append(age)
     friends.append({'name': name,'age':age})
-
 print(friends)
 print(age_list)
 midlle_age = sum(age_list)/N
 print(midlle_age)
-max_name = len(friends[0]['name'])
+max_name = friends[0]['name']
 for i in friends:
-    if max_name<len(i['name']):
+    if len(max_name)<len(i['name']):
         max_name = i['name']
 print(max_name)
